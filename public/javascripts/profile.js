f87eabac9f8052f88171ac9356469228737c6828
BLACK <blackkitten010@gmail.com>
2022-04-27 02:20:05 +0700

    form validate profile

---
+++ public/javascripts/profile.js
@@ -0,0 +1,135 @@
+$.validator.addMethod("noSpace",function(value, element) {
+    return value.indexOf(" ") < 0 && value != "";
+}, "ห้ามใช้ช่องว่าง"
+);
+$.validator.addMethod(
+    "regex",
+    function(value, element, regexp) 
+    {
+        console.log(value, regexp, regexp.test(value))
+        if (regexp.constructor != RegExp)
+            regexp = new RegExp(regexp);
+        else if (regexp.global)
+            regexp.lastIndex = 0;
+        return regexp.test(value);
+    },
+    "Please check your input."
+);
+$(function(){
+    var $Form1 = $("#editusername");
+    if($Form1.length){
+        $Form1.validate({
+            rules:{
+                newusername:{
+                    required: true,
+                    minlength:3,
+                    maxlength:10,
+                    regex: /^[A-Za-z_]{3,10}$/,
+                    noSpace:true
+                },
+                password:{
+                    required:true,
+                    // minlength:7,
+                    // maxlength:20,
+                    // regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_]).{7,20}$/,
+                    noSpace:true
+                }
+            },
+            messages:{
+                newusername:{
+                    required:'กรุณาป้อนชื่อผู้ใช้งาน',
+                    minlength:'ต้องมีอย่างน้อย 3 ตัวอักษร',
+                    maxlength:'ต้องมีอย่างมากไม่เกิน 10 ตัวอักษร',
+                    regex:'ชื่อผู้ใช้ต้องประกอบไปด้วย ตัวอักษรภาษาอังกฤษพิมเล็กหรือพิมใหญ่ เท่านั้น'
+                },
+                password:{
+                    required:'กรุณาป้อนรหัสผ่าน',
+                    // minlength:'รหัสผ่านต้องมีอย่างน้อย 7 ตัวอักษร',
+                    // maxlength:'รหัสผ่านต้องมีอย่างมากไม่เกิน 20 ตัวอักษร',
+                    // regex:'รหัสผ่านต้องประกอบไปด้วย ตัวเลข, ตัวอักษรภาษาอังกฤษพิมเล็ก และพิมใหญ่, อักขระพิเศษ'   
+                }
+            }
+        })
+    }
+
+    var $Form2 = $("#editpassword");
+    if($Form2.length){
+        $Form2.validate({
+            rules:{
+                newpassword:{
+                    required:true,
+                    minlength:7,
+                    maxlength:20,
+                    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_]).{7,20}$/,
+                    noSpace:true
+                },
+                confirmnewpassword:{
+                    required:true,
+                    equalTo:'#inputPassword3',
+                    noSpace:true
+                },
+                password:{
+                    required:true,
+                    // minlength:7,
+                    // maxlength:20,
+                    // regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_]).{7,20}$/,
+                    noSpace:true
+                }
+            },
+            messages:{
+                newpassword:{
+                    required:'กรุณาป้อนรหัสผ่าน',
+                    minlength:'รหัสผ่านต้องมีอย่างน้อย 7 ตัวอักษร',
+                    maxlength:'รหัสผ่านต้องมีอย่างมากไม่เกิน 20 ตัวอักษร',
+                    regex:'รหัสผ่านต้องประกอบไปด้วย ตัวเลข, ตัวอักษรภาษาอังกฤษพิมเล็ก และพิมใหญ่, อักขระพิเศษ (!@#$%^&*_)'
+                },
+                repassword:{
+                    required:'กรุณาป้อนรหัสผ่านอีกครั้ง',
+                    equalTo:'รหัสผ่านไม่เหมือนกัน'
+                },
+                password:{
+                    required:'กรุณาป้อนรหัสผ่าน',
+                    // minlength:'รหัสผ่านต้องมีอย่างน้อย 7 ตัวอักษร',
+                    // maxlength:'รหัสผ่านต้องมีอย่างมากไม่เกิน 20 ตัวอักษร',
+                    // regex:'รหัสผ่านต้องประกอบไปด้วย ตัวเลข, ตัวอักษรภาษาอังกฤษพิมเล็ก และพิมใหญ่, อักขระพิเศษ'
+                },
+                
+            }
+        })
+    }
+
+    var $Form3 = $("#editemail");
+    if($Form3.length){
+        $Form3.validate({
+            rules:{
+                newemail:{
+                    required:true,
+                    email:true,
+                    noSpace:true,
+                    regex: /^[A-Za-z0-9_]+\@[A-Za-z0-9_]+\.[A-Za-z0-9_]+/,
+                    
+                },
+                password:{
+                    required:true,
+                    // minlength:7,
+                    // maxlength:20,
+                    // regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_]).{7,20}$/,
+                    noSpace:true
+                }
+            },
+            messages:{
+                email:{
+                    required:'กรุณาป้อนอีเมลล์',
+                    email:'อีเมลล์ไม่ถูกต้อง',
+                    regex:'อีเมลล์ไม่ถูกต้อง'
+                },
+                password:{
+                    required:'กรุณาป้อนรหัสผ่าน',
+                    // minlength:'รหัสผ่านต้องมีอย่างน้อย 7 ตัวอักษร',
+                    // maxlength:'รหัสผ่านต้องมีอย่างมากไม่เกิน 20 ตัวอักษร',
+                    // regex:'รหัสผ่านต้องประกอบไปด้วย ตัวเลข, ตัวอักษรภาษาอังกฤษพิมเล็ก และพิมใหญ่, อักขระพิเศษ'
+                }
+            }
+        })
+    }
+})
--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -58,7 +58,7 @@ $(function(){
                     required:'กรุณาป้อนรหัสผ่าน',
                     minlength:'รหัสผ่านต้องมีอย่างน้อย 7 ตัวอักษร',
                     maxlength:'รหัสผ่านต้องมีอย่างมากไม่เกิน 20 ตัวอักษร',
-                    regex:'รหัสผ่านต้องประกอบไปด้วย ตัวเลข, ตัวอักษรภาษาอังกฤษพิมเล็ก และพิมใหญ่, อักขระพิเศษ'
+                    regex:'รหัสผ่านต้องประกอบไปด้วย ตัวเลข, ตัวอักษรภาษาอังกฤษพิมเล็ก และพิมใหญ่, อักขระพิเศษ ((!@#$%^&*_))'
                     
                 },
                 repassword:{
@@ -75,64 +75,3 @@ $(function(){
     }
     
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.getElementById('form');
-// const username = document.getElementById('inputUsername3');
-// const password = document.getElementById('inputPassword3');
-// const confirmpassword = document.getElementById('inputPasswordConfirm');
-// const email = document.getElementById('inputEmail3');
-
-// form.addEventListener('submit',function(e){
-//     e.preventDefault();
-//     // console.log("Submit")
-//     if(username.value == ""){
-//         console.log('None');
-//         showerorr_message(username,"กรุณาป้อนชื่อผู้ใช้งาน");
-//     }
-//     else{
-//         console.log("complete");
-//         return_normal(username);
-//     }
-
-// });
-
-
-
-
-
-// function showerorr_message(input,message){
-//     const control = input.parentElement;
-//     control.className = 'message erorr';
-//     const small = control.querySelector('small');
-//     small.innerText = message;
-    
-// }
-
-// function return_normal(input){
-//     const control = input.parentElement;
-//     control.className = 'message';
-// }
-
-
-
-
